46355ec13bb7fad8d2016ec205a7c4c71d272261
Jeff Reiffers <jeffreiffers@gmail.com>
2024-10-28 14:09:21 +0100

    chore: removed unused code

--- fdk_atlas_dcat_service/app.py
+++ fdk_atlas_dcat_service/app.py
@@ -16,16 +16,8 @@ from datacatalogtordf import Catalog, Dataset
 from fastapi import Body, Depends, FastAPI, HTTPException, status
 from fastapi.security.api_key import APIKey
 
-from fdk_rdf_parser.classes.exceptions import (
-    MissingResourceError,
-    MultipleResourcesError,
-    ParserError,
-)
-
 from fdk_atlas_dcat_service.config import setup_logging
 
-from fdk_atlas_dcat_service.model import resource_type_map
-from fdk_atlas_dcat_service.service import ParsedReturnType, parse_resource
 
 def azuread_auth(tenant_id: str, client_id: str, client_secret: str, resource_url: str):
     """
--- fdk_atlas_dcat_service/service.py
+++ fdk_atlas_dcat_service/service.py
@@ -1,35 +1,7 @@
 """Function for doing parse job"""
 
 from typing import Any, Callable, Dict
-from fdk_rdf_parser import (
-    parse_dataset_as_dict,
-    parse_concept_as_dict,
-    parse_information_model_as_dict,
-    parse_service_as_dict,
-    parse_event_as_dict,
-    parse_dataservice_as_dict,
-)
+
 
 from fdk_atlas_dcat_service.model import ResourceEnum
 
-ParsedReturnType = Dict[str, Any]
-
-parser_func_map: Dict[
-    ResourceEnum,
-    Callable[
-        [str],
-        ParsedReturnType,
-    ],
-] = {
-    ResourceEnum.DATASET: parse_dataset_as_dict,
-    ResourceEnum.DATA_SERVICE: parse_dataservice_as_dict,
-    ResourceEnum.CONCEPT: parse_concept_as_dict,
-    ResourceEnum.INFORMATION_MODEL: parse_information_model_as_dict,
-    ResourceEnum.SERVICE: parse_service_as_dict,
-    ResourceEnum.EVENT: parse_event_as_dict,
-}
-
-
-def parse_resource(rdf_data: str, resource_type: ResourceEnum) -> ParsedReturnType:
-    """Parses RDF data according to the given resource type and returns it as a JSON string"""
-    return parser_func_map[resource_type](rdf_data)
